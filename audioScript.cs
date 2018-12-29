fc4b5b024a79aabc7b973b05c11059b0553cd6ba
Nitesh Kumar <36387036+Nit5292@users.noreply.github.com>
2018-12-29 19:37:42 +0530

    audio folder

---
+++ audioScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<AudioSource>().Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<AudioSource>().Pause();
+        }
+        else
+        {
+            GetComponent<AudioSource>().Play();
+        }
+	}
+}
